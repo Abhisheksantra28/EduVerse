c626d3312e363aa42456168c5a83ccbbd3d4fbb3
Abhishek Santra <abhisheksantra28@gmail.com>
2023-11-16 09:21:10 +0530

    create register api and route

--- Server/src/controllers/user.js
+++ Server/src/controllers/user.js
@@ -1,4 +1,20 @@
-export const createUser = async(req,res,next)=>{
+import { asyncHander } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
-    
-}
+export const createUser = asyncHander(async (req, res, next) => {
+  const { username, email, phone, password } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    return res.status(400).json({
+      success: false,
+      message: "User Already Exists",
+    });
+  }
+
+  const newUser = await User.create({ username, email, phone, password });
+  res.status(201).json({
+    success: true,
+    message: "User Registered Successfully !!",
+  });
+});
--- Server/src/routes/user.js
+++ Server/src/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
+
 import { createUser } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/register", createUser);
+router.post("/register", createUser);
 
 export default router;
